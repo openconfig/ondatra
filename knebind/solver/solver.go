7acb4c4c9e519e46d2debf5b2d8b9cd6fbf5eaef
biplamal <athena@biplabondatra.ccu.is.keysight.com>
2022-06-07 08:41:01 +0000

    debugging added

--- knebind/solver/solver.go
+++ knebind/solver/solver.go
@@ -105,6 +105,8 @@ func Solve(tb *opb.Testbed, topo *tpb.Topology) (*binding.Reservation, error) {
 		s.intf2Intf[intfZ] = intfA
 	}
 
+	fmt.Println(&s)
+
 	a, err := s.solve()
 	if err != nil {
 		return nil, err
